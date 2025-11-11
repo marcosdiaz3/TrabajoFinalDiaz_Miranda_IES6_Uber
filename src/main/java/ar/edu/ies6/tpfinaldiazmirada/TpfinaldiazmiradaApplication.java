a2d074533b131f13d6579b75a1b0e3195c9a44ce
marcosdiaz3 <benjadiaz334>
2025-11-11 12:11:02 -0300

    primer commit inicio de proyecto  Diaz Miranda

---
+++ src/main/java/ar/edu/ies6/tpfinaldiazmirada/TpfinaldiazmiradaApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.ies6.tpfinaldiazmirada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpfinaldiazmiradaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpfinaldiazmiradaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/ies6/tpfinaldiazmirada/TpfinaldiazmiradaApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.ies6.tpfinaldiazmirada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpfinaldiazmiradaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
